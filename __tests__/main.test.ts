ae79fd2c6d4f0577276faa16eb8784e26bfc2b15
Deepak Kumar <deepak94.mce@gmail.com>
2021-06-18 00:56:37 -0400

    Update main.test.ts

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -18,7 +18,7 @@ test('wait 500 ms', async () => {
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
+  process.env['INPUT_MINUTES'] = '5'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {

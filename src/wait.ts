7708b99bf5703d379590f7e43ee3998a73b9d7d0
Deepak Kumar <deepak94.mce@gmail.com>
2021-06-19 00:11:44 -0400

    Update wait.ts

--- src/wait.ts
+++ src/wait.ts
@@ -1,7 +1,7 @@
 export async function wait(milliseconds: number): Promise<string> {
   return new Promise(resolve => {
     if (isNaN(milliseconds)) {
-      throw new Error('milliseconds not a number')
+      throw new Error('Not a number')
     }
     setTimeout(() => resolve('done!'), milliseconds)
   })
